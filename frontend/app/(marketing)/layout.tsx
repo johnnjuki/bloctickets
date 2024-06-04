b677cf3f50ed2b09b86703e4275e889d3a041853
John Njuki <johnnjuki.dev@gmail.com>
2024-06-04 07:41:12 +0300

    optimize for minipay

--- frontend/app/(marketing)/layout.tsx
+++ frontend/app/(marketing)/layout.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  const [isEthereumAvailable, setIsEthereumAvailable] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.ethereum) {
+      if (window.ethereum.isMinipay) {
+
+        setIsEthereumAvailable(false);
+      }
+    }
+  }, []);
+
+  if (isEthereumAvailable) {
+    return <div>YES</div>;
+  }
+
+  return <div>NO</div>
+
+  // return <>{children}</>;
 }
