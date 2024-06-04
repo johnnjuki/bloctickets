4d962cf2b75c07ac59931a5db19eb8823eb7034d
John Njuki <johnnjuki.dev@gmail.com>
2024-06-04 08:01:46 +0300

    optimize for minipay

--- frontend/app/(marketing)/layout.tsx
+++ frontend/app/(marketing)/layout.tsx
@@ -3,14 +3,23 @@
 import React, { useEffect, useState } from 'react';
 import { useMinipay } from '../hooks/useMinipay';
 
+function isOperaMobile() {
+  const userAgent = navigator.userAgent;
+  const isOpera = userAgent.indexOf('OPR') > -1;
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(userAgent);
+
+  return isOpera && isMobile;
+}
+
+
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const isMinipay = useMinipay();
+  // const isMinipay = useMinipay();
 
-  if (isMinipay) {
+  if (isOperaMobile()) {
     return <p>YES</p>
   } else {
     return <p>NO</p>
--- frontend/app/hooks/useMinipay.ts
+++ frontend/app/hooks/useMinipay.ts
@@ -1,11 +1,17 @@
+// hooks/useMinipay.ts
 import { useEffect, useState } from 'react';
 
 export const useMinipay = (): boolean => {
   const [isMinipay, setIsMinipay] = useState(false);
 
   useEffect(() => {
-    const userAgent = navigator.userAgent || navigator.vendor;
-    setIsMinipay(/Minipay/i.test(userAgent));
+    // Check if the browser is Opera and contains a specific Minipay marker
+    const userAgent = navigator.userAgent;
+    const isOpera = userAgent.indexOf('OPR') > -1;
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(userAgent);
+    if (isMinipay) {
+      setIsMinipay(true);
+    }
   }, []);
 
   return isMinipay;
